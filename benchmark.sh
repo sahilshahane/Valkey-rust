80515b14f893e72c631229471e64a10e36824a26
sahilshahane <sahilpshahane123@gmail.com>
2025-11-22 20:01:14 +0530

    feat: add variable support for benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,12 @@
-tail_n=16
-head_n=10
-running_time=30
-n_clients=100
+tail_n=${TAIL_N:-16}
+head_n=${HEAD_N:-10}
+running_time=${RUNNING_TIME:-30}
+n_clients=${N_CLIENTS:-12}
 
 cargo build --release --bin load_test > /dev/null
+
+echo "No. of Clients : ${n_clients}"
+echo "Runtime duration : ${running_time}"
 
 echo "Running PUT-ALL test (disk-bound)..."
 ./target/release/load_test http://localhost:4000 $n_clients $running_time putall | tail -n $tail_n | head -n $head_n && echo "\n"
@@ -17,5 +20,5 @@ echo "Running GET-POPULAR test (cache-bound)..."
 echo "Running GET+PUT test (mixed workload)..."
 ./target/release/load_test http://localhost:4000 $n_clients $running_time getput | tail -n $tail_n | head -n $head_n && echo "\n"
 
-echo "Running STRESS test (maximum throughput)..."
-./target/release/load_test http://localhost:4000 $n_clients $running_time stress | tail -n $tail_n | head -n $head_n
+# echo "Running STRESS test (maximum throughput)..."
+# ./target/release/load_test http://localhost:4000 $n_clients $running_time stress | tail -n $tail_n | head -n $head_n
